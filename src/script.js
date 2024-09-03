0f238945c58ba9e8d15314ef0491e4751e259a82
Mcthouacbb <Mcthouacbb@gmail.com>
2024-09-03 15:08:35 +0000

    Show time of in-progress sorts

--- src/script.js
+++ src/script.js
@@ -150,10 +150,10 @@ function testRender() {
 
     time += dt * timeScale;
     for (const sortRenderer of sortRenderers) {
+        for (const ctx of sortRenderer.contexts) {
+            timeElems[ctx.region].innerText = `Time: ${sortRenderer.time()}ms`;
+        }
         if (sortRenderer.done) {
-            for (const ctx of sortRenderer.contexts) {
-                timeElems[ctx.region].innerText = `Time: ${sortRenderer.time()}ms`
-            }
             continue;
         }
         renderer.render(sortRenderer.getNextRender(time));
